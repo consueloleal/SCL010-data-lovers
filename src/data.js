b0cbe45d433b9ea275e16609c7fb2a3f1eac00db
Consuelo Leal <consuelo.leal@outlook.com>
2019-07-10 08:18:00 -0400

    finale function for filter data and sort data

--- src/data.js
+++ src/data.js
@@ -5,7 +5,6 @@ const filterData = (data, condition) => {
   for (var key in condition) {
     
     if (condition[key] === "All")
-      
       delete condition[key]
   }
 
@@ -18,13 +17,49 @@ const filterData = (data, condition) => {
   });
 };
 
+const sortData = (data, sortBy, sortOrder) => {
+  
+  let ordenaPersonaje = data;
+  if (sortOrder == "a-z"){
+    ordenaPersonaje.sort((a,b)=> {
+      if (a[sortBy] < b[sortBy]) {return -1;}
+      if (a[sortBy] > b[sortBy]) {return  1;}
+      return 0;
+    })
+  }
 
-
-const sortData = (data, sortBy, sortOrder) => {};
-
+  if (sortOrder == "z-a"){
+    ordenaPersonaje.sort((a,b)=> {
+      if (a[sortBy] > b[sortBy]) {return -1;}
+      if (a[sortBy] < b[sortBy]) {return  1;}
+      return 0;
+    })
+  }
+  return ordenaPersonaje;
+};
 
 const computeStats = (data) => {
+  
+  // con esto se obtienen las especies y generos, lo cual podría servir para calcular
+  // el porcentaje de cada tipo de acuerdo a lo que se está mostrando (ya sea si se filtra o no)
+  var species = data.map(character => character["species"])
+  var gender  = data.map(character => character["gender"])
 
+
+  // Ejemplo POKEMON, se calculaba altura y pesos
+
+  // var hAverage, maxH, wAverage, maxW;
+
+  // var heights = data.map(monster => parseFloat(monster["height"]));
+  // var weights = data.map(monster => parseFloat(monster["weight"]));
+
+  // hAverage = (heights.reduce(function(a, b){ return a + b; }) / data.length).toFixed(2);
+  // wAverage = (weights.reduce(function(a, b){ return a + b; }) / data.length).toFixed(2);
+
+  // maxH = Math.max(...heights);
+  // maxW = Math.max(...weights);
+
+  // return {Altura_Promedio: hAverage + " m", Peso_Promedio: wAverage + " kg", Altura_Máxima: maxH + " m", Peso_Máximo: maxW + " kg"};
 };
 
 window.filterData = filterData;
--- src/main.js
+++ src/main.js
@@ -1,82 +1,65 @@
-
-
-//Primero creo el acceso a la data del proyecto (Visto en el archivo data rickandmorty.js)
-const accessData = () => {
-const rymData = RICKANDMORTY.results;
-return rymData;
-};
-
 const resultsNode = document.getElementById("result");
 //Mostrando todas las tarjetas de los personajes, con imagen, nombre, genero y especie
 function ups() {
-   const perData = accessData();
-    for (i=0; i<perData.length; i++){
+    for (i=0; i<rymData.length; i++){
        document.getElementById("result").innerHTML +=`
        <div class="tarjetas">
-       <img src="${perData[i].image}" alt="">
-       <p>Nombre: ${perData[i].name}</p>
-       <p>Género: ${perData[i].gender}</p>
-       <p>Especie: ${perData[i].species}</p>
+       <img src="${rymData[i].image}" alt="">
+       <p>Nombre: ${rymData[i].name}</p>
+       <p>Género: ${rymData[i].gender}</p>
+       <p>Especie: ${rymData[i].species}</p>
        </div>`;
    }
 }
 ups();
 
-let filteredData, Data ; 
+let filteredData, data; 
 
+//se obtiene el id de el selector para luego aplicar un elemento de escucha  
+document.getElementById('filterOrden').addEventListener("change",()=>{
+    filteredData = window.filterData(rymData, {gender: filterGender.value, species: filterSpecies.value})
+    rymStats     = window.computeStats(filteredData);
+    
+    clean();
+    if (filterOrden.value !== "Normal")
+      filteredData = sortData(filteredData, "name", filterOrden.value)
+    showFilterCharacter(filteredData);
+});
 
 document.getElementById('filterGender').addEventListener("change", ()=>{
     filteredData = window.filterData(rymData, {gender: filterGender.value, species: filterSpecies.value});
+    rymStats     = window.computeStats(filteredData);
+    
+    clean();
+    if (filterOrden.value !== "Normal")
+      filteredData = sortData(filteredData, "name", filterOrden.value)
     showFilterCharacter(filteredData);
-    debugger;
-    //debugger;
-    //clean();
 }); 
+
 document.getElementById('filterSpecies').addEventListener("change",()=>{
-    filteredData = window.filterData(rymData, {species: filterSpecies.value, gender: filterGender.value})
-    console.log(filteredData);
-  });
-//se obtiene el id de el selector para luego aplicar un elemento de escucha  
-document.getElementById('filterOrden').addEventListener("change",()=>{
-    Data = window.sortData(filteredData, "name", filterOrden.value );
+    filteredData = window.filterData(rymData, {gender: filterGender.value, species: filterSpecies.value})
+    rymStats     = window.computeStats(filteredData);
+    
+    clean();
+    if (filterOrden.value !== "Normal")
+      filteredData = sortData(filteredData, "name", filterOrden.value)
+    showFilterCharacter(filteredData);
 });
 
 function clean(){
   while (resultsNode.firstChild) {
     resultsNode.removeChild(resultsNode.firstChild);
   }
+}
 
 function showFilterCharacter(data){
-  debugger;
-    //se crea un div para que contendra al personaje que actual
   data.forEach(function(character){
-    const characterNode = document.createElement("div"); 
-    characterNode.id =character.name;
-    characterNode.className = "characterNode";
-    debugger;
-
-  result.innerHTML+='<div class="wrap">'
-                              '<div class="card-wrap">'+
-                                '<div class="card" style="width: 15rem;">'+ 
-                                 '<section class="front">'+
-                                    '<h5 class="card-title">' + character.name + '</h5>'+
-                                  '</section>'+
-                                  '<section class="back">'+
-                                    '<div class="card-body">'+
-                                      '<img id="image"src=" ${character[i].image} "></img>' +
-                                      '<span class="card-span">Nombre: </span><em>'+ character.name +'</em>'+
-                                      '<span class="card-span">Estado:</span><em>' + character.status + '</em>'+
-                                      '<span class="card-span">Género: </span><em>' + character.gender+'</em>'+ 
-                                      '<span class="card-span">Ultima locación: </span><em>' + character.location +'</em>'+
-                                    '</div>' +
-                                  '</section>'+ 
-                              '</div>'
-
-  //results.appendChild(characterNode);
+    document.getElementById("result").innerHTML +=`
+       <div class="tarjetas">
+       <img src="${character.image}" alt="">
+       <p>Nombre: ${character.name}</p>
+       <p>Género: ${character.gender}</p>
+       <p>Especie: ${character.species}</p>
+       </div>`;
   }); 
 };
-
-  /*while (rootStatNode.firstChild) {
-    rootStatNode.removeChild(rootStatNode.firstChild);
-  }*/
-
