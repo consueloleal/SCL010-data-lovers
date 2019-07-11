094a3657096b50574ef2129c6b37ab5a58c1fc78
Consuelo Leal <consuelo.leal@outlook.com>
2019-07-11 15:55:15 -0400

    add function for show computeStats

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const resultsNode = document.getElementById("result");
+const statsNode   = document.getElementById("stats");
 //Mostrando todas las tarjetas de los personajes, con imagen, nombre, genero y especie
-const rymData = window.RICKANDMORTY.results;
+const rymData = RICKANDMORTY.results;
 function ups() {
     for (let i=0; i<rymData.length;  i++){
        document.getElementById("result").innerHTML +=`
@@ -12,46 +13,57 @@ function ups() {
        <p>Estado: ${rymData[i].status} </p>
        </div>`;
    }
+   characterStats = computeStats(rymData);
+   showStatsCharacter(characterStats);
 }
 ups();
 
 let filteredData;
 
 
-//se obtiene el id de el selector para luego aplicar un elemento de escucha  
+//se obtiene el id de el selector para luego aplicar un elemento de escucha
 document.getElementById('filterOrden').addEventListener("change",()=>{
-    filteredData = window.filterData(rymData, {gender: window.filterGender.value, species: window.filterSpecies.value})
-    //rymStats     = window.computeStats(filteredData);
-    
+    filteredData = filterData(rymData, {gender: filterGender.value, species: filterSpecies.value})
+    rymStats     = computeStats(filteredData);
+
     clean();
-    if (window.filterOrden.value !== "Normal")
-      filteredData = window.sortData(filteredData, "name", window.filterOrden.value)
+    if (filterOrden.value !== "Normal")
+      filteredData = sortData(filteredData, "name", filterOrden.value)
     showFilterCharacter(filteredData);
+    characterStats = computeStats(filteredData);
+    showStatsCharacter(characterStats);
 });
 
 document.getElementById('filterGender').addEventListener("change", ()=>{
-    filteredData = window.filterData(rymData, {gender: window.filterGender.value, species: window.filterSpecies.value});
-    //rymStats     = window.computeStats(filteredData);
-    
+    filteredData = filterData(rymData, {gender: filterGender.value, species: filterSpecies.value});
+    rymStats     = computeStats(filteredData);
+
     clean();
-    if (window.filterOrden.value !== "Normal")
-      filteredData = window.sortData(filteredData, "name", window.filterOrden.value)
+    if (filterOrden.value !== "Normal")
+      filteredData = sortData(filteredData, "name", filterOrden.value)
     showFilterCharacter(filteredData);
-}); 
+    characterStats = computeStats(filteredData);
+    showStatsCharacter(characterStats);
+});
 
 document.getElementById('filterSpecies').addEventListener("change",()=>{
-    filteredData = window.filterData(rymData, {gender: window.filterGender.value, species: window.filterSpecies.value})
-    //rymStats     = window.computeStats(filteredData);
-    
+    filteredData = filterData(rymData, {gender: filterGender.value, species: filterSpecies.value})
+    rymStats     = computeStats(filteredData);
+
     clean();
-    if (window.filterOrden.value !== "Normal")
-      filteredData = window.sortData(filteredData, "name", window.filterOrden.value)
+    if (filterOrden.value !== "Normal")
+      filteredData = sortData(filteredData, "name", filterOrden.value)
     showFilterCharacter(filteredData);
+    characterStats = computeStats(filteredData);
+    showStatsCharacter(characterStats);
 });
 
 function clean(){
   while (resultsNode.firstChild) {
     resultsNode.removeChild(resultsNode.firstChild);
+  }
+  while (statsNode.firstChild) {
+    statsNode.removeChild(statsNode.firstChild);
   }
 }
 
@@ -65,5 +77,14 @@ function showFilterCharacter(data){
        <p>Especie: ${character.species}</p>
        <p>Estado: ${character.status} </p>
        </div>`;
-  }); 
+  });
 }
+
+function showStatsCharacter(stats){
+  for (var key in stats){
+    const statNode = document.createElement("div");
+    statNode.className = "statNode col-sm-3";
+    statNode.innerHTML = "<span class='statLabel'>" + key.replace(new RegExp("_", 'g'), " ") + ":</span> " + stats[key];
+    statsNode.appendChild(statNode);
+  }
+};
